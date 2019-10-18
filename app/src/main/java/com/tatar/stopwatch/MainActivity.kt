30fc3a8a3809f1408241205171fb3f700823d059
tartarJR <mustafaogun.ozturk@gmail.com>
2019-10-18 16:46:11 +0300

    Refactor MainActivity and use data binding

--- app/src/main/java/com/tatar/stopwatch/MainActivity.kt
+++ app/src/main/java/com/tatar/stopwatch/MainActivity.kt
@@ -8,60 +8,70 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.tatar.stopwatch.databinding.ActivityMainBinding
 
-private const val TAG = "MainActivity"
-
 class MainActivity : AppCompatActivity() {
 
-    private var hasStarted = false
+    private var hasStarted = false // TODO use ViewModel and DataBinding
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var timeWorker: TimeWorker
+    private lateinit var stopwatchWorker: StopwatchWorker
 
     private val uiHandler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
-            binding.timeTextView.text = msg.obj.toString()
+            binding.currentTime = msg.obj.toString()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        prepareBindings()
         prepareTimeWorker()
         prepareViews()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        timeWorker.quit()
+        stopwatchWorker.quit()
+    }
+
+    private fun prepareBindings() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.currentTime = "00:00:00:00" // TODO use ViewModel
     }
 
     private fun prepareTimeWorker() {
-        timeWorker = TimeWorker()
-        timeWorker.setUiHandlerHandler(uiHandler)
+        stopwatchWorker = StopwatchWorker()
+        stopwatchWorker.setUiHandler(uiHandler)
     }
 
     private fun prepareViews() {
         binding.startStopBtn.setOnClickListener setOnClickListener@{
             if (hasStarted) {
-                timeWorker.stopCounting()
-                binding.startStopBtn.text = getString(R.string.start_stop_btn_start_txt)
-                binding.resetBtn.isEnabled = true
-                hasStarted = false
-
+                stopStopwatch()
                 return@setOnClickListener
             }
 
-            timeWorker.startCounting()
-            binding.startStopBtn.text = getString(R.string.start_stop_btn_stop_txt)
-            binding.resetBtn.isEnabled = false
-            hasStarted = true
+            startStopwatch()
         }
 
         binding.resetBtn.setOnClickListener {
-
+            stopwatchWorker.resetCounting()
         }
+    }
+
+    private fun stopStopwatch() {
+        stopwatchWorker.stopCounting()
+        binding.startStopBtn.text = getString(R.string.start_stop_btn_start_txt)
+        binding.resetBtn.isEnabled = true
+        hasStarted = false
+    }
+
+    private fun startStopwatch() {
+        stopwatchWorker.startCounting()
+        binding.startStopBtn.text = getString(R.string.start_stop_btn_stop_txt)
+        binding.resetBtn.isEnabled = false
+        hasStarted = true
     }
 }
